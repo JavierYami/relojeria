c1839786fd40e52c901a3fcc1b9a39906eecd91b
JavierYami <javieryami1@outlook.com>
2024-03-26 17:46:47 -0600

    se creó estilo para el botoón desactivado

--- client/src/components/myTextInput.jsx
+++ client/src/components/myTextInput.jsx
@@ -6,7 +6,7 @@ const MyTextInput = ({ label, ...props }) => {
       <>
         <div className="flex w-full flex-col">          
           <label className="mb-1 font-bold text-gray-900" htmlFor={props.id || props.name}>{label}</label>
-          <input className="border border-gray-100 bg-brown-50 rounded-sm p-1" {...field} {...props} />
+          <input className="border border-brown-100 bg-brown-50 rounded-sm p-1" {...field} {...props} />
           {meta.touched && meta.error ? (
             <div className="error text-sm text-danger-600">{meta.error}</div>
           ) : null}
