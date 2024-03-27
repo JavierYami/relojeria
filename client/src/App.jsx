5dc18a88c4d0722dad50cad30b3cf94f49b53e5a
JavierYami <javieryami1@outlook.com>
2024-03-27 14:52:06 -0600

    instalación de redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import SignUp from "./views/SignUp";
+import { Route, Routes } from "react-router-dom";
 
 function App () {
 
     return (
         <div className="bg-gray-200 h-screen flex justify-center items-center p-10">
-            <SignUp></SignUp>
+            <Routes>
+                <Route path="/SignUp" Component={SignUp}/>
+            </Routes>
         </div>
     )
 
